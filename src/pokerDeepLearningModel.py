8d79f60226a3cefe7a591a8b503a9c9ae22a8963
EC2 Default User <ec2-user@ip-172-31-8-198.us-west-2.compute.internal>
2021-02-09 04:18:10 +0000

    Temporary tryout of Naive_Poker_Calculator.

--- src/pokerDeepLearningModel.py
+++ src/pokerDeepLearningModel.py
@@ -9,17 +9,25 @@ Created on Mon Aug 20 21:33:07 2018
 from pathlib import Path
 import random
 import numpy as np
-import tensorflow as tf
+import tensorflow.compat.v1 as tf
 from pokerDeck import pokerDeck
 from pokerProbabilities import fetchProbabilityArray, statusDictToInputArray, simulateProbability
 from pokerCombinatorics import findHandStatus
 from pokerNeuralNetwork import probabilityApproximator
+from pokerDeepLearningModelLib import mock_array
+from pokerDeepLearningModelLib import simulationDeck
+from pokerDeepLearningModelLib import evaluationDeck
+from pokerDeepLearningModelLib import tableEvaluationDeck
+import pokerDeepLearningModelLib
+
+
+tf.disable_eager_execution()
 
 
 
 ### Variables to toggle ###
 
-num_data_points = 1000           # The number of instances we want in our sample dataset
+num_data_points = 100            # The number of instances we want in our sample dataset
 use_existing_model = True        # Do you want to use a pre-computed model or train a new one?
 inference_sample_size = 100      # Size of sample to test the model on
 
@@ -39,84 +47,36 @@ for i in range(num_data_points):
     cleanDeck = sampleDeck.currentDeck[2:] # After we take the two hole cards we remove them from the deck
     
     sampleTableCards.append(cleanDeck[:random.choice(tableCardsOptions)])
-    
-    
-
-
-### Below we create a mock array to get the dimensionality (These are all the inputs for our model) ###
-    
-mock_array = ['prob1', 'prob2', 'prob3', 'prob4', 'prob5', 'prob6', 'prob7', 'prob8', 
-              'pairStatusHand', 'suitedStatusHand', 'straightGapStatusHand', 'lowCardHand', 'highCardHand',
-              'pairStatusTable', 'suitedStatusTable', 'straightGapStatusTable', 'tripleStatusTable', 'TwoPairStatusTable', 
-              'FullHouseStatusTable', 'RunnerRunnerStatusTable', 'SingleRunnerStatusTable', 
-              'Straight - Table', 'Flush - Table', 'Four of a Kind - Table', 'Straight Flush - Table',
-              'preflopOneHot', 'flopOneHot', 'turnOneHot', 'riverOneHot']
-
-### We create multiple decks that serve different purposes when creating the input vectors ###
-
-simulationDeck = pokerDeck()
-evaluationDeck = pokerDeck()
-tableEvaluationDeck = pokerDeck()
 
 ### We initialize empty arrays for the inputs and the labels ###
 
 probabilityInputList = np.zeros((len(sampleHands), len(mock_array)))
 probabilityList = np.zeros((len(sampleHands),2))
 
+
+
 ### We run through the loop to generate the inputs and labels to use during training ###
 
 for j in range(len(sampleHands)):
-    
+
     ### We print which iteration we are on every 100 instances to keep track of our status ###
     if j % 100 == 0:
         print('You are at iteration {}'.format(j))
+    print('sampleHand: %s' % sampleHands[j])
+    print('sampleTable: %s' % sampleTableCards[j])
 
-    """
-    Below we calculate the probability of getting various hand rankings
-    
-    If we achieved a certain hand, all hands at least as good as it get a 1 assigned
-    All other hands get a probability assigned
-    """
     
     evaluationDeck.shuffleDeck(); evaluationDeck.table = sampleTableCards[j]
-    sampleCurrentHand, _ = evaluationDeck.evaluateHand(sampleHands[j])
-    sampleCurrentRanking = simulationDeck.handRanking(sampleCurrentHand)
-    probArray = fetchProbabilityArray(sampleHands[j], sampleTableCards[j], sampleCurrentRanking)
-    
-    ### Below we find the state of our hole cards using a combination of variables ###
-    
-    preflopStatusDict = findHandStatus(sampleHands[j], [])
-    statusArray = statusDictToInputArray(preflopStatusDict, "Hand", sampleHands[j], None)
-    
-    ### Below we find the state of the board cards using a combination of variables ###
-    
-    if len(sampleTableCards[j]) != 0:
-        tableStatusDict = findHandStatus(sampleTableCards[j], [])
-    else:
-        tableStatusDict = {}
-    tableStatusArray = statusDictToInputArray(tableStatusDict, "Table", sampleTableCards[j], tableEvaluationDeck)
-    
-    ### Below we create a one-hot encoding of the betting round state ###
-    
-    if len(sampleTableCards[j]) == 0:
-        tableStatus = [1,0,0,0]
-    elif len(sampleTableCards[j]) == 3:
-        tableStatus = [0,1,0,0]
-    elif len(sampleTableCards[j]) == 4:
-        tableStatus = [0,0,1,0]
-    elif len(sampleTableCards[j]) == 5:
-        tableStatus = [0,0,0,1]
+
         
     ### Below we combine all of the different states into one input vector (which is assigned to the input array) ###
     
-    probabilityInputList[j,] = np.concatenate((probArray, statusArray, tableStatusArray, tableStatus))
+    probabilityInputList[j,] = pokerDeepLearningModelLib.getProbabilityInputList(sampleHands[j], sampleTableCards[j])
     
     ### Below we simulate the probability to use as our label ###
     
     temp_prob_array = simulateProbability(sampleHands[j], sampleTableCards[j], simulationDeck, 1000)
     probabilityList[j,] = temp_prob_array[-1,]
-
-
 
 
 """
@@ -127,6 +87,7 @@ We have the option to load the pre-computed neural network or to train a new mod
 
 tf.reset_default_graph()
 sess = tf.Session()
+
 
 if use_existing_model:
     my_probability_file = Path("Probability Model/checkpoint")
@@ -145,9 +106,11 @@ else:
 
 ### We split the data into a training and testing set ###
 
-n_training_set = 900
+n_training_set = 90
 train_X, test_X = probabilityInputList[:n_training_set,], probabilityInputList[n_training_set:,]
 train_Y, test_Y = probabilityList[:n_training_set,], probabilityList[n_training_set:,]
+
+print("train_X: %s" % train_X)
 
 if use_existing_model == False:    
     
@@ -159,6 +122,7 @@ if use_existing_model == False:
 ### Perform Inference ###
 
 test_predictions = sess.run(probabilityFunction.approximate_probability, {probabilityFunction.inputs: test_X[:inference_sample_size,]})
+print('test_predictions: %s' % test_predictions)
 test_labels = test_Y[:inference_sample_size,]
 test_MAE = np.mean(abs(test_predictions - test_labels), axis = 0)
 
